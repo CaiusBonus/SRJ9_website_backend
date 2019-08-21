fbc20e17b5c82177be69d36e29a31ab55cefca36
Jakub Juhas <juhas.jugi@gmail.com>
2019-08-21 21:49:39 +0200

    Add initial spring project

---
+++ src/main/kotlin/com/srj9/Srj9/Srj9Application.kt
@@ -0,0 +1,11 @@
+package com.srj9.Srj9
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Srj9Application
+
+fun main(args: Array<String>) {
+	runApplication<Srj9Application>(*args)
+}
