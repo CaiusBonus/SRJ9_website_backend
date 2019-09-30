2bb67dd5ad945329b55cf0172df8c586054c54de
Dominik Juhás <dominik.juhas@gmail.com>
2019-09-30 21:32:02 +0200

    Added CORS configuration.

--- src/main/kotlin/com/srj9/controller/GymReservationRestController.kt
+++ src/main/kotlin/com/srj9/controller/GymReservationRestController.kt
@@ -14,37 +14,31 @@ class GymReservationRestController {
     @Autowired
     lateinit var gymReservationService: GymReservationService
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/gym_reservation")
     fun getAllGymReservations(): List<GymReservation> {
         return gymReservationService.getAllGymReservations()
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/gym_reservation/{reservationId}")
     fun getSingleGymReservation(@PathVariable reservationId: Long): GymReservation {
         return gymReservationService.getSingleGymReservation(reservationId)
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @PostMapping("/gym_reservation")
     fun createGymReservation(@Valid @RequestBody gymReservation: GymReservation) : GymReservation {
         return gymReservationService.createGymReservation(gymReservation)
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/gym_reservation/current_week")
     fun getAllReservationsForCurrentWeek(): List<GymReservation> {
         return gymReservationService.getAllReservationsForCurrentWeek()
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @PutMapping("/gym_reservation/{reservationId}")
     fun updateSingleGymReservation(@RequestBody gymReservation: GymReservation, @PathVariable reservationId: Long): ResponseEntity<GymReservation> {
         return gymReservationService.updateExistingGymReservation(gymReservation,reservationId)
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @DeleteMapping("/gym_reservation/{reservationId}")
     fun deleteExistingGymReservation(@PathVariable reservationId: Long): ResponseEntity<Void> {
         return gymReservationService.deleteGymReservation(reservationId)
--- src/main/kotlin/com/srj9/controller/LaundryReservationRestController.kt
+++ src/main/kotlin/com/srj9/controller/LaundryReservationRestController.kt
@@ -15,46 +15,38 @@ class LaundryReservationRestController {
     @Autowired
     lateinit var laundryReservationService: LaundryReservationService
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/laundry_reservation")
     fun getAllLaundryReservations() : List<LaundryReservation> {
         return laundryReservationService.getAllLaundryReservations()
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/laundry_reservation/{reservationId}")
     fun getSingleLaundryReservation(@PathVariable reservationId: Long): LaundryReservation {
         return laundryReservationService.getSingleLaundryReservation(reservationId)
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @PostMapping("/laundry_reservation")
     fun createLaundryReservation(@Valid @RequestBody laundryReservation: LaundryReservation): LaundryReservation {
         return laundryReservationService.createLaundryReservation(laundryReservation)
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/laundry_reservation/current_week")
     fun getAllReservationsForCurrentWeek(): List<LaundryReservation> {
         return laundryReservationService.getAllReservationsForCurrentWeek()
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @GetMapping("/laundry_reservation/available_hours")
     fun getAllAvailableHoursForSpecificDate(@PathVariable laundryDate: Date): String {
         return ""
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @PutMapping("/laundry_reservation/{reservationId}")
     fun updateExistingReservation(@RequestBody laundryReservation: LaundryReservation, @PathVariable reservationId: Long): ResponseEntity<LaundryReservation> {
         return laundryReservationService.updateExistingLaundryReservation(laundryReservation,reservationId)
     }
 
-    @CrossOrigin(origins = arrayOf("http://localhost:4200"))
     @DeleteMapping("/laundry_reservation/{reservationId}")
     fun deleteExistingReservation(@PathVariable reservationId: Long): ResponseEntity<Void> {
         return laundryReservationService.deleteLaundryReservation(reservationId)
     }
-
 }
