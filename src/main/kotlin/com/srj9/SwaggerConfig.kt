5db97e7db57f7c1c89fbebd83c15fe780826d1df
Jakub Juhas <juhas.jugi@gmail.com>
2019-09-30 12:53:40 +0200

    Added swagger config

---
+++ src/main/kotlin/com/srj9/SwaggerConfig.kt
@@ -0,0 +1,34 @@
+package com.srj9
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.regex
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+
+
+@Configuration
+@EnableSwagger2
+class SwaggerConfig {
+    @Bean
+    fun productApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.srj9"))
+                .paths(PathSelectors.regex("/api.*"))
+                .build()
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("Reservation System SRJ9").description("API for creating and managing reservations").version("1.0.0").contact(Contact("Jakub Juhas", "", "jakub.juhas@student.tuke.sk")).build()
+    }
+}
